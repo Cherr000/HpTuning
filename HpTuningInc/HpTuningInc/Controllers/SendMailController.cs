054dd6d5ec81cee4ba0aaa09c55957ce391be06b
cherr000 <chudue@hotmail.com>
2017-04-13 14:57:56 -0500

    Can Now Send Email To Remind Customer

---
+++ HpTuningInc/HpTuningInc/Controllers/SendMailController.cs
@@ -0,0 +1,53 @@
+﻿using HpTuningInc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace HpTuningInc.Controllers
+{
+    public class SendMailController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: SendMail
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ReminderPage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ReminderPage(Reminder reminder)
+        {
+            if (ModelState.IsValid)
+            {
+                WebMail.Send(reminder.Email
+                            , "HPTuningInc - Reminder Service Due",
+                            "Hi, This Is Just A Reminder To Let You Know That You Are Due For Service" + "<br />" + "<a href=\"http://localhost:40592/Appointment/Create\">Click Here To Make A Appointment</a>"
+                            , null
+                            , null
+                            , null
+                            , true
+                            , null
+                            , null
+                            , null
+                            , null
+                            , null
+                            , reminder.Email);
+                return RedirectToAction("SendConfirm");
+            }
+            return View();
+        }
+
+        public ActionResult SendConfirm()
+        {
+            return View();
+        }
+    }
+}
---
+++ HpTuningInc/HpTuningInc/Models/Reminder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HpTuningInc.Models
+{
+    public class Reminder
+    {
+        public string  Name { get; set; }
+        public string Email { get; set; }
+    }
+}
